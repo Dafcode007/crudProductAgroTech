bc54f9caca63d3816065642956a92c62a0c5e001
Dafcode007 <dafcode07@gmail.com>
2021-11-26 05:39:38 -0500

    create and iniatilize project spring boot crud product agrotechAPP

---
+++ src/main/java/com/example/crudProductAgroTech/CrudProductAgroTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudProductAgroTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudProductAgroTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProductAgroTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudProductAgroTech/CrudProductAgroTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudProductAgroTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProductAgroTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

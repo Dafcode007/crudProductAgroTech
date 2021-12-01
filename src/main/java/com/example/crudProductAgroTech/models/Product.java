4b95c4a1acc46321b8146d084d1ac262ec92e50e
Dafcode007 <dafcode07@gmail.com>
2021-12-01 15:38:15 -0500

    create model product

---
+++ src/main/java/com/example/crudProductAgroTech/models/Product.java
@@ -0,0 +1,60 @@
+package com.example.crudProductAgroTech.models;
+
+import org.springframework.data.annotation.Id;
+
+public class Product {
+    @Id
+    private String code;
+    private String name;
+    private String category;
+    private Integer quantity;
+    private Integer price;
+
+    public Product(String code, String name, String category, Integer quantity, Integer price) {
+        this.code = code;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
